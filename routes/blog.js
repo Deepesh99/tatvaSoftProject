b642ed7aa7456baba7dc041b75e933c877279662
Deepesh Nair <deepesh99.nair@gmail.com>
2023-10-27 14:48:36 +0530

    project setup

---
+++ routes/blog.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router
+.get('/blog',)
+.post('/create',)
+.patch('/update',)
+.delete('/delete',);
+
+module.exports = router;
